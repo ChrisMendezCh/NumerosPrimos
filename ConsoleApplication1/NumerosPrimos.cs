2051cfdd8624f5f135bd2465f49cf4ef8db07896
POCHO <POCHO@ALFWPC>
2024-02-04 12:29:45 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApplication1/NumerosPrimos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static bool EsPrimo(int n)
+    {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static List<int> NumerosPrimos(int hasta)
+    {
+        List<int> primos = new List<int>();
+
+        for (int i = 2; i <= hasta; i++)
+        {
+            if (EsPrimo(i))
+                primos.Add(i);
+        }
+
+        return primos;
+    }
+
+    static void Main()
+    {
+        int limiteSuperior = 100;
+        Console.WriteLine("Números primos hasta " + limiteSuperior + ":");
+        foreach (int primo in NumerosPrimos(limiteSuperior))
+        {
+            Console.Write(primo + " ");
+        }
+    }
+}
